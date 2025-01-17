e3176047c44a84e6f13663f581007a664c5e8732
Darya Lok <daryalokteva@gmail.com>
2025-01-17 13:57:09 +0100

    homework task1

---
+++ week10_homework1/index.js
@@ -0,0 +1,14 @@
+
+
+const img = document.getElementById("picture");
+
+
+function getPrevPicture() {
+    img.src = "./assets/1.jpg";
+
+}
+
+function getNextPicture() {
+    img.src = "./assets/2.jpg";
+
+}
